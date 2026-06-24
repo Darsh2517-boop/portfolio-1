f6d46d85c387b91cdbe70ba6616fee25d52f9bfa
Darsh2517-boop <trivedidarsh250107@gmail.com>
2026-06-24 12:47:22 +0530

    upload file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      <navbar />
+      <hero />
+      <about />
+      <skills />
+      <projects />
+      <contact />
+      <footer />
     </>
   );
 }
